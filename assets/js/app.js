1e903bf03c2788b8c1cd55ffc5272081e0518015
LiquidArtDevelopers <aranaz@gmail.com>
2024-06-18 10:59:08 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+/* import "./_toggle.js"; */
+/* import "./_scroll.js"; */
+
+
+const midocumentoHTML = document;
+midocumentoHTML.addEventListener('DOMContentLoaded',()=>{ 
+    /* todo lo que meta aquí dentro se ejecutará cuando se termine de cargar el DOM */
+});
